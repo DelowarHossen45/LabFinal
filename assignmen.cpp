6d545ab9c5bb13eebb2dbc7eac9f867236a798d2
Ebrahim <ebrahimali.cse.ru@gmail.com>
2018-05-01 12:04:55 +0600

    Multiplication

--- assignmen.cpp
+++ assignmen.cpp
@@ -1,6 +1,15 @@
 #include<bits/stdc++.h>
 using namespace std;
+void MUL()
+{
+	int var1,var2;
+	cout<<"Enter Two Variable:"<<endl;
+	cin>>var1>>var2;
+	cout<< var1*var2<<endl;
+
+}
 inr main()
 {
-	
+
+	MUL();
 }
